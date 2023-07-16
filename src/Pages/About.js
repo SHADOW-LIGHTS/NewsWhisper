b91e815080892e6634321ea3f44f2370f5360fed
PRUTHVI <108618163+SHADOW-LIGHTS@users.noreply.github.com>
2023-07-16 15:01:52 +0530

    Update About.js

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,36 +1,56 @@
 import React from "react";
-import "./Docs.css";
+import "./About.css";
 
 export default function About() {
   return (
     <div className="docs-container">
       <h1>
-        Voice-Driven News Application: Your Hands-Free Gateway to the World of News
+        Voice-Driven News Application: Your Hands-Free Gateway to the World of
+        News
       </h1>
 
       <div className="section">
         <h2>About Voice-Driven News Application</h2>
         <p>
-          Welcome to the Voice-Driven News Application, an innovative and user-centric platform that revolutionizes the way you access and interact with news content. Our application combines cutting-edge technologies, including NewsAPI and Alan AI, to provide a seamless and hands-free experience, making staying informed easier than ever before.
+          Welcome to the Voice-Driven News Application, an innovative and
+          user-centric platform that revolutionizes the way you access and
+          interact with news content. Our application combines cutting-edge
+          technologies, including NewsAPI and Alan AI, to provide a seamless and
+          hands-free experience, making staying informed easier than ever
+          before.
         </p>
       </div>
 
       <div className="section">
         <h2>Our Mission</h2>
         <p>
-          Our mission is to make news browsing accessible and efficient for everyone. We believe that the power of voice commands can enhance the way we interact with technology and consume information. With the Voice-Driven News Application, we aim to empower users to access the latest news articles, headlines, and more, simply by using their voice.
+          Our mission is to make news browsing accessible and efficient for
+          everyone. We believe that the power of voice commands can enhance the
+          way we interact with technology and consume information. With the
+          Voice-Driven News Application, we aim to empower users to access the
+          latest news articles, headlines, and more, simply by using their
+          voice.
         </p>
-    <p>
-          Key Features
-        </p>
+        <p>Key Features</p>
         <ul>
-          <li>Voice-Controlled News: No more typing or scrolling. Navigate through news articles and headlines effortlessly with just your voice.</li>
-          <li>Filtered Searches: Find news articles by latest updates, categories, specific terms, and preferred news sources through intuitive voice commands.</li>
           <li>
-            Hands-Free Experience: Whether you're driving, cooking, or simply multitasking, the Voice-Driven News Application allows you to stay informed without lifting a finger.
+            Voice-Controlled News: No more typing or scrolling. Navigate through
+            news articles and headlines effortlessly with just your voice.
           </li>
           <li>
-            Natural Language Processing: Our advanced AI-powered system understands natural language, ensuring a smooth and natural interaction.
+            Filtered Searches: Find news articles by latest updates, categories,
+            specific terms, and preferred news sources through intuitive voice
+            commands.
+          </li>
+          <li>
+            Hands-Free Experience: Whether you're driving, cooking, or simply
+            multitasking, the Voice-Driven News Application allows you to stay
+            informed without lifting a finger.
+          </li>
+          <li>
+            Natural Language Processing: Our advanced AI-powered system
+            understands natural language, ensuring a smooth and natural
+            interaction.
           </li>
         </ul>
       </div>
@@ -38,11 +58,15 @@ export default function About() {
       <div className="section">
         <h2>Getting Started</h2>
         <p>
-          To get started, all you need to do is obtain API keys from NewsAPI and Alan AI. With a quick setup process, you'll be ready to explore the world of news through voice commands.
+          To get started, all you need to do is obtain API keys from NewsAPI and
+          Alan AI. With a quick setup process, you'll be ready to explore the
+          world of news through voice commands.
         </p>
-    <h2>Join Us in the Future of News Browsing</h2>
+        <h2>Join Us in the Future of News Browsing</h2>
         <p>
-          Experience the future of news browsing with the Voice-Driven News Application. Embrace the power of voice and embark on a journey of seamless news exploration. Stay informed, stay connected!
+          Experience the future of news browsing with the Voice-Driven News
+          Application. Embrace the power of voice and embark on a journey of
+          seamless news exploration. Stay informed, stay connected!
         </p>
       </div>
     </div>
